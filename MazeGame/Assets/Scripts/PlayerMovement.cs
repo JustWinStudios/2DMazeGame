4c4b8ebe67bdc41e6871a63ea8041d93ef2db998
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-01-26 08:00:23 -0500

    programmed an x/y axis movement script

--- MazeGame/Assets/Scripts/PlayerMovement.cs
+++ MazeGame/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+    private Vector2 movement;
+    
     void Update()
     {
-        
+        //Input:
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += (Vector3)movement * moveSpeed * Time.fixedDeltaTime;
     }
 }
