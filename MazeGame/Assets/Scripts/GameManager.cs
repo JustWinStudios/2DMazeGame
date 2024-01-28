1c074fad8d19f122540ea0bcf39c83e4098c3d11
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-01-28 18:13:39 -0500

    Created base for a game manager

---
+++ MazeGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private int pizzasCollected = 0;
+    private int pizzasToCollect = Random.Range(10, 16);
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CollectPizza(int amount)
+    {
+        pizzasCollected += amount;
+        CheckLevelCompletion();
+    }
+
+    void CheckLevelCompletion()
+    {
+        if (pizzasCollected >= pizzasToCollect)
+        {
+            //Load next level or Scene
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        pizzasCollected = 0;
+        pizzasToCollect = Random.Range(10, 16);
+
+        // Organize the scenes based on how you want to progress the maps
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
