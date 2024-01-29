365858298eceb142128c22b0b323ec51c3b409b5
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-01-29 16:01:34 -0500

    completed instructions tab setup
    Code will open/close instructions tab. Code will also hide menu buttons when instructions tab is open and show menu buttons when instructions tab is closed.

--- MazeGame/Assets/Scripts/InstructionsManager.cs
+++ MazeGame/Assets/Scripts/InstructionsManager.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class InstructionsManager : MonoBehaviour
 {
     public GameObject instructionsPanel; // Assign in Inspector
+    public GameObject buttonsObject; // Assign to Buttons Family
 
     public void ToggleInstructions()
     {
+        bool isInstructionsActive = !instructionsPanel.activeSelf;
         instructionsPanel.SetActive(!instructionsPanel.activeSelf);
+
+        //Toggles visibility of the main menu buttons
+        if (buttonsObject != null)
+        {
+            buttonsObject.SetActive(!isInstructionsActive);
+        }
+    }
+
+    public void HideInstructions()
+    {
+        instructionsPanel.SetActive(false); // Closes Instructions panel
+        buttonsObject.SetActive(true); // Shows main menu buttons
     }
 }
